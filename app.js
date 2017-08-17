abaa7d38ec6ed38596e3302b54cb240abeb61f74
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-17 16:11:49 -0400

    cleanup

--- app.js
+++ app.js
@@ -45,22 +45,4 @@ app.post("/complete", function (req, res) {
   res.redirect('/');
 })
 
-// app.get('/', function(req, res){
-//   // Set 'action' to '/'
-//   var html = '<form action="/" method="post">' +
-//              '<h1>User Name</h1>' +
-//              '<p>Enter your email</p>' +
-//              '<input type="text" name="email" placeholder="email address" />' +
-//              '<button type="submit">Submit</button>' +
-//          '</form>';
-//   res.send(html);
-// });
-//
-// // Receives data from form (action='/')
-// // 'req.body' now contains form data.
-// app.post('/', function(req, res){
-//   var email = req.body.email;
-//   var html = '<p>Your user name is: </p>' + email;
-//   res.send(html);
-// });
 app.listen(3000);
